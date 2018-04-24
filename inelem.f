673fa7fef65544a3ec539fcccbb88abac83c8757
maranGit <maran.accnt1@gmail.com>
2018-04-24 11:14:15 -0400

    finite element input

--- inelem.f
+++ inelem.f
@@ -29,7 +29,7 @@ c
 c                       translate the list of elements input on
 c                       this line.
 c     
-      errmsg = 0
+      errnum = 0
       do while (.true.)
         call readsc()
         call scan()
@@ -42,7 +42,7 @@ c
 
         ! successfully read an element list
         call backsp(1)
-        call ElemProps(intlst,lenst)
+        call ElemProps(intlst,lenlst)
       enddo
       call backsp(1)
 
@@ -54,13 +54,16 @@ c     ****************************************************************
 c     *                      subroutine ElemProps                    *
 c     *                   read element properties                    * 
 c     ****************************************************************
-      subroutine ElemProps()
+      subroutine ElemProps(intlst,lenlst)
       implicit none
+
+c                         global
+      integer :: intlst(*), lenlst
 
 c                         local
       integer :: nc, currMat, iplist, icn, elem
       character :: unknown*24,lname*80,mname*24
-      logical, external :: endcrd, matchs, label
+      logical, external :: endcrd, matchs, label, scanms
       logical :: matFound
 c
       do while ( .not. endcrd(dum) )
@@ -84,12 +87,12 @@ c
           matFound = .false.
           do while( mat_props(currMat)%assigned )
             if( scanms( mat_props(currMat)%matnam, mname, 24 ) ) then
-              matFount = .true.
+              matFound = .true.
               exit
             end if
             currMat = currMat + 1
           end do
-          if( .not. found ) then
+          if( .not. matFound ) then
             call errmsg(12,dum,mname,dumr,dumd)
             return
           end if
@@ -106,7 +109,7 @@ c           matList(elem) = currMat
         ! unknown terms in element properties
         else
           call entits(unknown,nc)
-          call errmsg(10,dum,unknown(1:nc),dunr,dumd)
+          call errmsg(10,dum,unknown(1:nc),dumr,dumd)
         end if
       end do
       return
