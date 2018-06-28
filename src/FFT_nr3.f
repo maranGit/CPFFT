0f959f0109d17c4979f630d6db91652677610235
maranGit <maran.accnt1@gmail.com>
2018-06-28 16:20:57 -0400

    P driven, not verified

--- src/FFT_nr3.f
+++ src/FFT_nr3.f
@@ -5,7 +5,7 @@ c     *                 subroutine     FFT_nr3                       *
 c     *                                                              *
 c     *                       written by : RM                        *
 c     *                                                              *
-c     *                   last modified: 1/22/18                     *
+c     *                   last modified: 6/27/18                     *
 c     *                                                              *
 c     *                  global Newton-Raphson loop                  *
 c     *                                                              *
@@ -20,9 +20,9 @@ c
 
 c                    local
       real(8), parameter :: zero = 0.0D0, one = 1.0D0, mone = -1.0D0
-      real(8) :: Fnorm, resfft, DbarF(9), barF(9), barF_t(9)
-      real(8) :: C_homo(81)
-      integer :: step, iiter, ii
+      real(8) :: P_bar(9), resPbar(3), DbarF(9), barF(9), barF_t(9)
+      real(8) :: FBC(9), PBC(9), C_homo(81), Fnorm, resfft, N3_dbl
+      integer :: step, ii, jj, iiter_EBC, iiter_NBC
       logical :: debug
       real(8), external :: dnrm2
 c
@@ -31,6 +31,8 @@ c
       data DbarF  /zero,zero,zero,zero,zero,zero,zero,zero,zero/
       data debug  /.false./
 c
+      N3_dbl = dble(N3)
+c
 c                         global step increment
 c 
       do step = 1, nstep
@@ -38,55 +40,113 @@ c
         write(*,*) "Now starting step ",step
         ltmstp = step
 c
-c              average strain increment
+c                         extract boundary condition
 c
-        barF  = BC_all(1:9, step)
-        DbarF = barF - barF_t
+        PBC = zero
+        FBC = zero
+        do ii = 1, nstrs
+          if ( isNBC(ii) ) then
+            PBC( ii ) = BC_all( ii, step )
+          else
+            FBC( ii ) = BC_all( ii, step )
 c
-c     initial residual: distribute "barF" over grid using K4
-c
-        b = zero
-        dFm = zero
-        do ii = 1, 9
-          dFm(1:N3, ii) = DbarF(ii)
+c           DbarF(EBC) = FBC, DbarF(NBC) = extrapolation
+            DbarF( ii ) = FBC( ii ) - barF_t( ii )
+          end if
         end do
-        call G_K_dF(dFm, b, .true.)
-        call dscal(N3*nstrs, mone, b(1,1), 1)
-        Fn1 = Fn1 + dFm
-        Fnorm = dnrm2(N3*nstrs, Fn1(1,1), 1)
-        resfft = Fnorm
-        iiter = 0
-
-c     iterate as long as iterative update does not vanish
-        do while ( .true. )
-          call fftPcg(b, dFm, tolPCG, out) ! results stored in dFm in mod_fft
+        barF = barF_t + DbarF
+        iiter_NBC = 0
 c
+c                    NR loop for natural boundary condition
+c
+        do while ( .true. )
+c
+c         initial residual: distribute "barF" over grid using K4
+c
+          b = zero
+c
+c         dFm = repmat( DbarF, N3, 1 )
+          dFm = zero
+          do ii = 1, 9
+            dFm(1:N3, ii) = DbarF(ii)
+          end do
           call daxpy(N3*nstrs, one, dFm(1,1), 1, Fn1(1,1), 1)
-          
-          call drive_eps_sig( step, iiter )
-          
-          call G_K_dF(Pn1, b, .false.)
+c
+c         get b, solve for first dFm
+c
+          call G_K_dF(dFm, b, .true.)
           call dscal(N3*nstrs, mone, b(1,1), 1)
-          resfft = dnrm2(N3*nstrs, dFm(1,1), 1)
-          write(*,*) resfft/Fnorm
-          if ((resfft/Fnorm < tolNR) .and. (iiter > 0)) exit
-          if ( iiter .eq. maxIter ) then
-            write(out,9999)
+          call fftPcg(b, dFm, tolPCG, out)
+          call daxpy(N3*nstrs, one, dFm(1,1), 1, Fn1(1,1), 1)
+c
+c         initialize for most current boundary condition
+c
+          Fnorm = dnrm2(N3*nstrs, Fn1(1,1), 1)
+          resfft = one
+          iiter_EBC = 0
+c
+c         iterate as long as iterative update does not vanish
+c
+          do while ( resfft .gt. tolNR )
+
+            call drive_eps_sig( step, iiter_EBC )
+            call G_K_dF(Pn1, b, .false.)
+            call dscal(N3*nstrs, mone, b(1,1), 1)
+            call fftPcg(b, dFm, tolPCG, out)
+            call daxpy(N3*nstrs, one, dFm(1,1), 1, Fn1(1,1), 1)
+            resfft = dnrm2(N3*nstrs, dFm(1,1), 1) / Fnorm
+            write(*,*) resfft
+            if ( iiter_EBC .eq. maxIter ) then
+              write(out,9999)
+              call die_abort()
+            endif
+            iiter_EBC = iiter_EBC + 1
+
+          enddo ! end N-R loop
+c
+c                   check if P_bar converges to PBC
+c
+          resPbar = zero
+          P_bar = zero
+          do ii = 1, nstrs
+            do jj = 1, N3
+              P_bar(ii) = P_bar(ii) + Pn1(jj,ii)
+            end do
+            P_bar(ii) = P_bar(ii) / N3_dbl
+            resPbar(2) = resPbar(2) + P_bar(ii) * P_bar(ii)
+c           || Pbar - PBC || only when PBC(ii) is prescribed
+            if( .not. isNBC(ii) ) cycle
+            resPbar(1) = resPbar(1) 
+     &                 + (P_bar(ii)-PBC(ii)) * (P_bar(ii)-PBC(ii))
+c
+          end do
+          resPbar(3) = sqrt( resPbar(1) / resPbar(2) )
+          if ( resPbar(3) .le. tolNR ) exit
+c
+c                 check if exceed maximum iteration
+c
+          if ( iiter_NBC .gt. maxIter ) then
+            write(out,9998)
             call die_abort()
           endif
-          iiter = iiter + 1
-          
-        enddo ! end N-R loop
+c
+c            if P_bar does not converge to PBC, update DbarF
+c            compute homogenized tangent stiffness
+c            b is borrowed as intermediate array to save memory!
+c
+          call tangent_homo( C_homo )
+          call NBC_update(C_homo(1),DbarF(1),P_bar(1),PBC(1),isNBC(1))
+c
+          iiter_NBC = iiter_NBC + 1
+
+        end do
 c
 c            update state variables
 c
+        DbarF(1:9) = barF(1:9) - barF_t(1:9)
         barF_t(1:9) = barF(1:9)
         call dcopy(N3*nstrs, Fn1(1,1), 1, Fn(1, 1), 1)
         call dcopy(N3*nstrs, Pn1(1,1), 1, Pn(1, 1), 1)
-c
-c            compute homogenized tangent stiffness
-c
-        call tangent_homo( C_homo )
 c
 c            update global variables in eleblocks
 c
@@ -104,6 +164,8 @@ c
       end if
       
  9999 format(/,'>> Error: Newton loop does not converge.')
+ 9998 format(/,'>> Error: Prescribed stress cannot be reached ',
+     &         'within given maximum iteration.'/)
       end subroutine
 c
 c     ****************************************************************
@@ -252,4 +314,67 @@ c
      &     /,10x,'returned the ERROR code:      ',i2)
  1001 format(/1x,'>>> pcg converged at iteration ', i4, 
      &            ' to a solution with relative residual ',e14.6)
+      end subroutine
+c
+c     ****************************************************************
+c     *                                                              *
+c     *                 subroutine   NBC_update                      *
+c     *                                                              *
+c     *                       written by : RM                        *
+c     *                                                              *
+c     *                   last modified: 6/28/18                     *
+c     *                                                              *
+c     *              update delta_F for prescribed P                 *
+c     *                                                              *
+c     ****************************************************************
+c
+      subroutine NBC_update( C_homo, DbarF, P_bar, PBC, isNBC )
+      implicit none
+      include 'common.main'
+c
+c                        global
+c
+      real(8), intent(in)  :: C_homo(nstrs,*), P_bar(*), PBC(*)
+      logical, intent(in)  :: isNBC(*)
+      real(8), intent(out) :: DbarF(*)
+c
+c                        local
+c
+      real(8) :: AAA(nstrs,nstrs), bbb(nstrs)
+      integer :: ipiv(nstrs), info, i
+      real(8), parameter :: zero = 0.0D0, one = 1.0D0
+c
+c                   form A matrix
+c
+      do i = 1, nstrs
+        AAA(1:nstrs,i) = C_homo(i,1:nstrs)
+      end do
+      do i = 1, nstrs
+        if ( isNBC(i) ) cycle
+        AAA(1:nstrs, i) = zero
+        AAA(i, 1:nstrs) = zero
+        AAA(i, i)       = one
+      end do
+c
+c                   form b vector
+c
+      bbb(1:nstrs) = PBC(1:nstrs) - P_bar(1:nstrs)
+c
+c                      solve
+c
+      call dgesv(nstrs,1,AAA(1,1),nstrs,ipiv(1),bbb(1),nstrs,info)
+      if ( info .ne. 0 ) then
+        write(out,*) ">>> Error: P_bar update failed"
+        call die_abort()
+      end if
+c
+c                   return DbarF
+c
+      DbarF(1:nstrs) = zero
+      do i = 1, nstrs
+        if ( .not. isNBC(i) ) cycle
+        DbarF(i) = bbb(i)
+      end do
+c
+      return
       end subroutine
